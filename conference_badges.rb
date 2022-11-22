561c55107107e14011db7fa84a867d688d59a3b7
QunFan <allenfan1012@gmail.com>
2022-11-22 11:27:25 +1100

    commit tree 1c347a96d71b88fc8f9ae3d4bb06fd73f9a4f123

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(array_names)
+    
+    array_names.map do |name|
+     "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(array_names)
+    array_names.map.with_index(1) do |name,room_number|
+     "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    end
+end
+
+def printer(array_names)
+    batch_badge_creator(array_names).each do |name|
+        puts name
+    end
+
+    assign_rooms(array_names).each do |assignment|
+        puts assignment
+    end
+
+end
